74cf497e63110e24781f8489715563e5a3079f3a
Chih-yu Huang <akahuang@google.com>
2011-10-07 18:44:53 +0800

    Rename function "setDegree" to "setOrientation".
    bug:5244256
    Change-Id: I6f5c96a5e1fefab5fa09a12fd3a0cf169bcbc6f5

--- src/com/ti/omap4/android/camera/ui/RotateLayout.java
+++ src/com/ti/omap4/android/camera/ui/RotateLayout.java
@@ -16,8 +16,6 @@
 
 package com.ti.omap4.android.camera.ui;
 
-import com.ti.omap4.android.camera.ui.Rotatable;
-
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
