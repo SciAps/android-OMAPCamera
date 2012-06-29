d9c5de66912d17933b52adebf19e2bebacc9ba15
Daniel Levin <dendy@ti.com>
2012-06-29 11:52:19 +0300

    Camera sounds
    android.hardware.CameraSound class is reworked and moved from private
    to public API in JB. New class name is android.media.MediaActionSound.
    
    Change-Id: Ie0ed3e5c05dd4e4e019ccfc7c94f32f9cf1476f1
    Signed-off-by: Daniel Levin <dendy@ti.com>

--- src/com/ti/omap4/android/camera/Camera.java
+++ src/com/ti/omap4/android/camera/Camera.java
@@ -45,9 +45,9 @@ import android.hardware.Camera.FaceDetectionListener;
 import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.Size;
-import android.hardware.CameraSound;
 import android.location.Location;
 import android.media.CameraProfile;
+import android.media.MediaActionSound;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -198,7 +198,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
     // generating thumbnails. This reduces the shot-to-shot time.
     private ImageSaver mImageSaver;
 
-    private CameraSound mCameraSound;
+    private MediaActionSound mCameraSound;
 
     private S3DViewWrapper s3dView;
     private boolean mS3dViewEnabled = false;
@@ -1455,7 +1455,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
 
     @Override
     public void playSound(int soundId) {
-        mCameraSound.playSound(soundId);
+        mCameraSound.play(soundId);
     }
 
     private boolean saveDataToFile(String filePath, byte[] data) {
@@ -1664,7 +1664,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         // Do this after starting preview because it depends on camera
         // parameters.
         initializeIndicatorControl();
-        mCameraSound = new CameraSound();
+        mCameraSound = new MediaActionSound();
 
         // Make sure preview is started.
         try {
