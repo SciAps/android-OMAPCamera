bb2018d3091eb0c30f8a6701d00a83fd6c9b9903
Aleksandar Dimitrov <x0129936@ti.com>
2012-10-11 16:57:25 +0300

    CameraOMAP_jb: Fix for building CameraTest
    Change-Id: I63dfefd0c711ce50ca472bfd8411ba39346349c8
    Signed-off-by: Aleksandar Dimitrov <x0129936@ti.com>

--- tests/Android.mk
+++ tests/Android.mk
@@ -11,7 +11,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := littlemock dexmaker
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := CameraTests
+LOCAL_PACKAGE_NAME := OMAPCameraTests
 
 LOCAL_INSTRUMENTATION_FOR := CameraOMAP
 
